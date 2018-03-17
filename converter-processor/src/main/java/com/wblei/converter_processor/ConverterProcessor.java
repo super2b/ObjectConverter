f4ce8a06d6f63cb646ce1c95fd5a5e66221b1df9
super2b <super2b@qq.com>
2018-03-17 23:44:25 +0800

    Add HellWorld.java code generation

--- converter-processor/src/main/java/com/wblei/converter_processor/ConverterProcessor.java
+++ converter-processor/src/main/java/com/wblei/converter_processor/ConverterProcessor.java
@@ -1,7 +1,11 @@
 package com.wblei.converter_processor;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import com.wblei.converter_annotation.Converter;
+import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
@@ -12,6 +16,7 @@ import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.MirroredTypeException;
 import javax.lang.model.type.TypeMirror;
@@ -22,13 +27,13 @@ import javax.tools.Diagnostic;
  * Created by superb2b on 13/03/2018.
  */
 @AutoService(Processor.class) public class ConverterProcessor extends AbstractProcessor {
-  private Filer mFiler;
+  private Filer filter;
   private Messager mMessager;
   private Elements mElementUtils;
 
   @Override public synchronized void init(ProcessingEnvironment processingEnvironment) {
     super.init(processingEnvironment);
-    mFiler = processingEnvironment.getFiler();
+    filter = processingEnvironment.getFiler();
     mMessager = processingEnvironment.getMessager();
     mElementUtils = processingEnvironment.getElementUtils();
   }
@@ -86,16 +91,32 @@ import javax.tools.Diagnostic;
       //
 
       //
-      log("类名:"+element.getSimpleName().toString());
+      try {
+        generateJavaClass("HelloWorld");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      log("类名:" + element.getSimpleName().toString());
       log("包名:" + element.getEnclosingElement().toString());
       log("注解的类:" + className);
     }
     return false;
   }
 
+  private void generateJavaClass(String clazzName) throws IOException {
+    MethodSpec main = MethodSpec.methodBuilder("main")
+        .addParameter(String[].class, "args")
+        .addStatement("$T.out.println($S)", System.class, clazzName)
+        .build();
+    TypeSpec typeSpec = TypeSpec.classBuilder("HelloWorld")
+        .addMethod(main)
+        .addModifiers(Modifier.FINAL, Modifier.PUBLIC)
+        .build();
+    JavaFile javaFile = JavaFile.builder("com.example.helloworld", typeSpec).build();
+    javaFile.writeTo(filter);
+  }
 
   private void log(String msg) {
     mMessager.printMessage(Diagnostic.Kind.NOTE, msg);
   }
-
 }
