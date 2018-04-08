4c4bb4abc67c5be9fecc9411d4950d98eba80dfd
super2b <super2b@qq.com>
2018-04-08 14:51:24 +0800

    add the annotation at UserWrapper.java

--- app/src/main/java/com/wblei/vo/UserWrapper.java
+++ app/src/main/java/com/wblei/vo/UserWrapper.java
@@ -7,6 +7,7 @@ import com.wblei.converter_annotation.PBField;
  * stand for http result.
  * Created by weibolei on 25/03/2018.
  */
+@Converter(source = User.class)
 public class UserWrapper extends CommonVo {
   private String password;
   private int age;
