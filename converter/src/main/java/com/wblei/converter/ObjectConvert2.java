7a5a691765a311b1001c775ccdde8189be500d29
super2b <super2b@qq.com>
2018-04-09 10:49:06 +0800

    add a method that can convert the list of source object

--- converter/src/main/java/com/wblei/converter/ObjectConvert2.java
+++ converter/src/main/java/com/wblei/converter/ObjectConvert2.java
@@ -1,5 +1,8 @@
 package com.wblei.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by weibolei on 27/03/2018.
  */
@@ -20,5 +23,27 @@ public class ObjectConvert2 {
       e.printStackTrace();
     }
     return null;
+  }
+
+  public static <T> List<T> tryToConvert(Class clazz, List<? extends Object> sourceOfList) {
+    List<T> resultList = new ArrayList<>();
+    try {
+      Class converterClazz = Class.forName(clazz.getName() + "_Converter");
+      Object obj = converterClazz.newInstance();
+      if (obj instanceof IConverter) {
+        for (Object sourceObj : sourceOfList) {
+          T t = (T)((IConverter)obj).convert(sourceObj);
+          resultList.add(t);
+        }
+      }
+
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InstantiationException e) {
+      e.printStackTrace();
+    }
+    return resultList;
   }
 }
